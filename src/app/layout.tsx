9bdeedf8d6f4b8ac543eff1954cba0e4a8dcdd3e
Dheeru Rajpoot <150139748+dheerurajpoot@users.noreply.github.com>
2025-04-20 17:42:15 +0530

    update analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,6 +62,22 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
+			<head>
+				<script
+					async
+					src='https://www.googletagmanager.com/gtag/js?id=G-VZ1TVC6LEG'
+				/>
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+							window.dataLayer = window.dataLayer || [];
+							function gtag(){dataLayer.push(arguments);}
+							gtag('js', new Date());
+							gtag('config', 'G-VZ1TVC6LEG');
+						`,
+					}}
+				/>
+			</head>
 			<body className={inter.className}>
 				<div className='relative flex min-h-screen flex-col'>
 					<Header />
