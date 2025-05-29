62c9466b083d1c1fc65088d21ff8c314ce1e4085
Dheeru Rajpoot <150139748+dheerurajpoot@users.noreply.github.com>
2025-05-29 17:05:39 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
 import { Toaster } from "react-hot-toast";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -62,23 +63,29 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
-			<head>
-				<script
-					async
-					src='https://www.googletagmanager.com/gtag/js?id=G-VZ1TVC6LEG'
-				/>
-				<script
-					dangerouslySetInnerHTML={{
-						__html: `
-							window.dataLayer = window.dataLayer || [];
-							function gtag(){dataLayer.push(arguments);}
-							gtag('js', new Date());
-							gtag('config', 'G-VZ1TVC6LEG');
-						`,
-					}}
-				/>
-			</head>
+			<head />
+			{/* Google Tag Manager */}
+			<Script id="gtm" strategy="afterInteractive">
+				{`
+				(function(w,d,s,l,i){
+					w[l]=w[l]||[];
+					w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+					var f=d.getElementsByTagName(s)[0],
+					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+					j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+					f.parentNode.insertBefore(j,f);
+				})(window,document,'script','dataLayer','GTM-M9QCDSB4');
+				`}
+			</Script>
 			<body className={inter.className}>
+			<noscript>
+				<iframe
+					src="https://www.googletagmanager.com/ns.html?id=GTM-M9QCDSB4"
+					height="0"
+					width="0"
+					style={{ display: 'none', visibility: 'hidden' }}
+				/>
+			</noscript>
 				<div className='relative flex min-h-screen flex-col'>
 					<Header />
 					{children}
